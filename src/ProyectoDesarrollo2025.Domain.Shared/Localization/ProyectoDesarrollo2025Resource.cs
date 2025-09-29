2a64d6b49687214b9f2210ad62525b76c3ba6310
alex16ramos <147538915+alex16ramos@users.noreply.github.com>
2025-09-29 02:38:47 -0300

    Delete src/ProyectoDesarrollo2025.Domain.Shared directory

--- src/ProyectoDesarrollo2025.Domain.Shared/Localization/ProyectoDesarrollo2025Resource.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Localization;
-
-namespace ProyectoDesarrollo2025.Localization;
-
-[LocalizationResourceName("ProyectoDesarrollo2025")]
-public class ProyectoDesarrollo2025Resource
-{
-
-}
--- src/ProyectoDesarrollo2025.Domain.Shared/ProyectoDesarrollo2025DomainErrorCodes.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ProyectoDesarrollo2025;
-
-public static class ProyectoDesarrollo2025DomainErrorCodes
-{
-    /* You can add your business exception error codes here, as constants */
-}
