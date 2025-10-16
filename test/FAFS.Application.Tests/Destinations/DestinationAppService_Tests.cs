b7628a25876b5bd7a1908ac893fc21436eed3ea6
alex1 <alex116ramos@gmail.com>
2025-10-16 17:56:53 -0300

    Creacion de GeoDbCitySearchService

--- test/FAFS.Application.Tests/Destinations/DestinationAppService_Tests.cs
+++ test/FAFS.Application.Tests/Destinations/DestinationAppService_Tests.cs
@@ -9,95 +9,33 @@ using FAFS.Application.Contracts.Destinations;
 
 namespace FAFS.Application.Tests.Destinations
 {
-    public class DestinationAppService_Tests : FAFSApplicationTestBase
+    public abstract class DestinationAppService_Tests : FAFSApplicationTestBase
     {
-        private readonly DestinationAppService _destinationAppService;
-        private readonly IRepository<Destination, Guid> _destinationRepository;
+       private readonly IDestinationAppService _service;
 
-        public DestinationAppService_Tests()
+        protected DestinationAppService_Tests()
         {
-            _destinationRepository = GetRequiredService<IRepository<Destination, Guid>>();
-            _destinationAppService = GetRequiredService<DestinationAppService>();
+            _service = GetRequiredService<IDestinationAppService>();
         }
 
         [Fact]
-        public async Task Should_Create_Destination_Successfully()
+        public async Task CreateAsync_ShouldReturnCreatedDestinationDto ()
         {
-            var input = new CreateUpdateDestinationDto
+            // Arrange
+            var createDto = new CreateUpdateDestinationDto
             {
-                Name = "Cataratas del Iguazú",
-                Country = "Argentina",
-                City = "Puerto Iguazú",
-                PhotoUrl = "http://example.com/photo.jpg",
-                Latitude = "-25.6953",
-                Longitude = "-54.4367"
+                Name = "Test Destination",
+                Country = "Test Location"
             };
-
-            var result = await _destinationAppService.CreateAsync(input);
-
+            // Act
+            var result = await _service.CreateAsync(createDto);
+            // Assert
             result.ShouldNotBeNull();
-            result.Name.ShouldBe("Cataratas del Iguazú");
-
-            var entity = await _destinationRepository.GetAsync(result.Id);
-            entity.Name.ShouldBe("Cataratas del Iguazú");
-        }
-
-        [Fact]
-        public async Task Should_Throw_Exception_When_Name_Is_Empty()
-        {
-            var input = new CreateUpdateDestinationDto
-            {
-                Name = "", // inválido
-                Country = "Argentina",
-                City = "Buenos Aires",
-                PhotoUrl = "http://example.com/photo.jpg",
-                Latitude = "-34.6037",
-                Longitude = "-58.3816"
-            };
-
-            await Assert.ThrowsAsync<AbpValidationException>(async () =>
-            {
-                await _destinationAppService.CreateAsync(input);
-            });
-        }
-
-        [Fact]
-        public async Task Should_Throw_Exception_When_Name_Is_Null()
-        {
-            var input = new CreateUpdateDestinationDto
-            {
-                Name = null!, // inválido
-                Country = "Argentina",
-                City = "Buenos Aires",
-                PhotoUrl = "http://example.com/photo.jpg",
-                Latitude = "-34.6037",
-                Longitude = "-58.3816"
-            };
-
-            await Assert.ThrowsAsync<AbpValidationException>(async () =>
-            {
-                await _destinationAppService.CreateAsync(input);
-            });
-        }
-
-        [Fact]
-        public async Task Should_Throw_Exception_When_Country_Is_Empty()
-        {
-            var input = new CreateUpdateDestinationDto
-            {
-                Name = "Destino X",
-                Country = "", // inválido
-                City = "Buenos Aires",
-                PhotoUrl = "http://example.com/photo.jpg",
-                Latitude = "-34.6037",
-                Longitude = "-58.3816"
-            };
-
-            await Assert.ThrowsAsync<AbpValidationException>(async () =>
-            {
-                await _destinationAppService.CreateAsync(input);
-            });
+            result.Id.ShouldNotBe(Guid.Empty);
+            result.Name.ShouldBe(createDto.Name);
+            result.Country.ShouldBe(createDto.Country);
         }
     }
 }
+
 
