99758e2a8b4decd5cb675cda7bf2bd23526fbe03
alex1 <alex116ramos@gmail.com>
2025-11-07 02:51:31 -0300

    Pruebas

---
+++ test/FAFS.Application.Tests/Destinations/DestinationRatingAppService_InMemory_Tests.cs
@@ -0,0 +1,40 @@
+﻿using FAFS.Application.Contracts.Destinations;
+using FAFS.Destinations;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Users;
+using Xunit;
+
+namespace FAFS.Application.Tests.Destinations
+{
+    public class DestinationRatingAppService_InMemory_Tests : FAFSApplicationTestBase
+    {
+        private readonly IDestinationRatingAppService _appService;
+
+        public DestinationRatingAppService_InMemory_Tests()
+        {
+            _appService = GetRequiredService<IDestinationRatingAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Filter_By_Current_User()
+        {
+            // Arrange
+            var currentUser = GetRequiredService<ICurrentUser>();
+            var userId = currentUser.Id ?? Guid.NewGuid();
+            var destinationId = Guid.NewGuid();
+
+            // Act
+            await _appService.RateDestinationAsync(destinationId, 5, "Increíble");
+
+            // Assert
+            var repo = GetRequiredService<IRepository<DestinationRating, Guid>>();
+
+            var ratings = await repo.GetListAsync();
+
+            ratings.ShouldAllBe(r => r.UserId == userId);
+        }
+    }
+}
---
+++ test/FAFS.Application.Tests/Destinations/DestinationRatingAppService_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAFS.Destinations
+{
+    internal class DestinationRatingAppService_Tests
+    {
+    }
+}
